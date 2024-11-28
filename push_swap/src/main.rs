7ff01b042aeff23057a47ca2822fe6574a795619
Arnaud Lier <arnaud@lier.link>
2024-11-28 11:36:49 +0100

    check push_swap checker output and use NonZero type for test count arg

--- push_swap/src/main.rs
+++ push_swap/src/main.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
 use rand_unique::{RandomSequence, RandomSequenceBuilder};
 use std::{
-    io::{Read, Write},
-    path::Path,
-    process::{Command, Stdio},
-    sync::{Arc, Mutex},
+    io::{Read, Write}, num::NonZeroU32, path::Path, process::{Command, Stdio}, sync::{Arc, Mutex}
 };
 
 use threadpool::ThreadPool;
@@ -12,6 +9,7 @@ use threadpool::ThreadPool;
 const RED: &str = "\x1B[1;31m";
 const GREEN: &str = "\x1B[1;32m";
 const RESET: &str = "\x1B[0m";
+const PUSH_SWAP_PATH: &str = "./push_swap";
 
 fn test(
     number_count: usize,
@@ -28,7 +26,7 @@ fn test(
         .collect::<Vec<String>>()
         .join(" ");
 
-    let mut push_swap = Command::new("./push_swap")
+    let mut push_swap: std::process::Child = Command::new(PUSH_SWAP_PATH)
         .arg(&arg)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -38,8 +36,10 @@ fn test(
         .arg(&arg)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()
         .expect("failed to execute checker");
+
     let mut push_swap_stdout = String::new();
     let mut push_swap_stderr = String::new();
     push_swap
@@ -54,15 +54,35 @@ fn test(
         .expect("failed to open push_swap stderr")
         .read_to_string(&mut push_swap_stderr)
         .expect("failed to read push_swap stderr");
-    if push_swap_stderr.contains("Error") {
+    if push_swap_stderr == "Error\n" {
         eprintln!("{RED}{arg} failed...{RESET}");
     }
+
     checker
         .stdin
         .take()
         .expect("failed to open checker stdin")
         .write_all(push_swap_stdout.as_bytes())
         .expect("failed to write to checker stdin");
+
+    let mut checker_stdout = String::new();
+    let mut checker_stderr = String::new();
+    checker
+        .stdout
+        .take()
+        .expect("failed to open checker stdout")
+        .read_to_string(&mut checker_stdout)
+        .expect("failed to read checker stdout");
+    checker
+        .stderr
+        .take()
+        .expect("failed to open checker stderr")
+        .read_to_string(&mut checker_stderr)
+        .expect("failed to read checker stderr");
+    if checker_stdout == "Error\n" || checker_stdout == "KO\n" {
+        eprintln!("{RED}{arg} failed...{RESET}");
+    }
+
     let instruction_count = push_swap_stdout.lines().count() as u32;
     *sum.lock().unwrap() += f64::from(instruction_count);
     let mut minimum: std::sync::MutexGuard<'_, u32> = minimum.lock().unwrap();
@@ -77,13 +97,13 @@ fn test(
     checker.wait().unwrap();
 }
 
-fn test_batch(test_count: u32, number_count: usize, objective: u32, checker_path: &'static str) {
+fn test_batch(test_count: NonZeroU32, number_count: usize, objective: u32, checker_path: &'static str) {
     println!("Testing {number_count} random numbers {test_count} time(s)...");
     let pool = ThreadPool::new(12);
     let sum = Arc::new(Mutex::new(0.0));
     let minimum = Arc::new(Mutex::new(u32::MAX));
     let maximum = Arc::new(Mutex::new(0u32));
-    for _ in 0..test_count {
+    for _ in 0..test_count.get() {
         let sum = Arc::clone(&sum);
         let minimum = Arc::clone(&minimum);
         let maximum = Arc::clone(&maximum);
@@ -96,7 +116,7 @@ fn test_batch(test_count: u32, number_count: usize, objective: u32, checker_path
     println!("Minimum: {} instructions", *minimum.lock().unwrap());
     println!(
         "Average: {} instructions",
-        *sum.lock().unwrap() / test_count as f64
+        *sum.lock().unwrap() / test_count.get() as f64
     );
     println!("Maximum: {} instructions", *maximum.lock().unwrap());
     if *maximum.lock().unwrap() > objective {
@@ -112,18 +132,14 @@ fn test_batch(test_count: u32, number_count: usize, objective: u32, checker_path
 #[command(version, about)]
 struct Args {
     #[arg(short, long)]
-    test_count: Option<u32>,
+    test_count: Option<NonZeroU32>,
     #[arg(short, long)]
     use_own_checker: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    let test_count = args.test_count.unwrap_or(100);
-    if test_count == 0 {
-        eprintln!("Zero is not allowed");
-        return;
-    }
+    let test_count = args.test_count.unwrap_or(100.try_into().unwrap());
     let mut checker_path: &'static str = if cfg!(target_os = "linux") {
         "./checker_linux"
     } else {
@@ -133,11 +149,11 @@ fn main() {
         checker_path = "./checker";
     }
     if !Path::new(checker_path).exists() {
-        eprintln!("{checker_path} doesn't exist");
+        eprintln!("{RED}{checker_path} doesn't exist.{RESET}");
         return;
     }
-    if !Path::new("./push_swap").exists() {
-        eprintln!("./push_swap doesn't exist");
+    if !Path::new(PUSH_SWAP_PATH).exists() {
+        eprintln!("{RED}{PUSH_SWAP_PATH} doesn't exist.{RESET}");
         return;
     }
     test_batch(test_count, 1, 0, checker_path);
